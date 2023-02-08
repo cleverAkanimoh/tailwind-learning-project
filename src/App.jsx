c8b48f1bd6764e698ef6e31b6fa295cdf0fa4220
cleverAkanimoh <crushclever1@gmail.com>
2023-02-08 15:06:17 +0100

    modified App.jsx in src

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
+import Footer from "./Components/Footer";
+import MiddleContent from "./Components/MiddleContent";
+import NavBar from "./Components/NavBar";
+
 export default function App() {
   return (
     <main>
-      
+      <NavBar />
+      <MiddleContent />
+      <Footer />
     </main>
   )
 }
