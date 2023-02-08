99eea42d6df22d30ce86ea295e75709d11cbf067
cleverAkanimoh <crushclever1@gmail.com>
2023-02-08 15:46:13 +0100

    changed 4 files

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -1,21 +1,20 @@
-import "../assets/index.css"
 
 export default function () {
     return (
-        <nav className="w-screen bg-red">
-            <div>
-                <div>
-                    <h3><strong>Job</strong><span>Search</span></h3>
-                    <h4>menu</h4>
+        <nav className="sm:w-full p-3 capitalize">
+            <div className="sm:flex items-center sm:justify-between">
+                <div className="sm:w-full flex justify-between">
+                    <h3><strong className="text-blue-700">Job</strong><span className="text-blue-700">Search</span></h3>
+                    <h4 className="sm:hidden">menu</h4>
                 </div>
-                <ul>
-                    <li>jobs</li>
-                    <li>companies</li>
-                    <li>about</li>
-                    <li>contact</li>
-                    <li>blog</li>
-                    <li>login</li>
-                    <li>register</li>
+                <ul className="sm:w-screen mt-3 sm:flex">
+                    <li className="sm:pr-3 text-gray-600 font-sans cursor-pointer">jobs</li>
+                    <li className="sm:pr-3 text-gray-600 font-sans cursor-pointer">companies</li>
+                    <li className="sm:pr-3 text-gray-600 font-sans cursor-pointer">about</li>
+                    <li className="sm:pr-3 text-gray-600 font-sans cursor-pointer">contact</li>
+                    <li className="sm:pr-3 text-gray-600 font-sans cursor-pointer">blog</li>
+                    <li className="sm:pr-3 text-gray-600 font-sans cursor-pointer">login</li>
+                    <li className="sm:pr-3 text-gray-600 font-sans cursor-pointer">register</li>
                 </ul>
             </div>
         </nav>
