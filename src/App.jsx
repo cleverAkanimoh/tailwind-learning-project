3c9ea1452084f1912b17308c69c0cca0c22c340d
cleverAkanimoh <crushclever1@gmail.com>
2023-02-08 11:34:00 +0100

    added 12 files

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+export default function App() {
+  return (
+    <>
+      <h1 className="text-3xl font-bold w-screen">
+        Hello world!
+      </h1>
+      <div className="w-screen h-screen bg-red-300">
+        <div className="w-9 h-7 bg-blue-100">Text</div>
+        <div className="w-5 h-6 bg-blue-200">Text</div>
+        <div className="w-5 h-6 bg-blue-300">Text</div>
+        <div className="w-5 h-6 bg-blue-400">Text</div>
+        <div className="w-5 h-6 bg-blue-500">Text</div>
+        <div className="w-5 h-6 bg-blue-600">Text</div>
+        <div className="w-5 h-6 bg-blue-700">Text</div>
+        <div className="w-5 h-6 bg-blue-800">Text</div>
+        <div className="w-5 h-6 bg-blue-900">Text</div>
+      </div>
+    </>
+  )
+}
