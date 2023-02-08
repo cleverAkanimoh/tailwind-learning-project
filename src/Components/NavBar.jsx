b6f09edbf612d752a667273a4a78100f6a87b887
cleverAkanimoh <crushclever1@gmail.com>
2023-02-08 15:14:56 +0100

    changed 5 files

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -1,7 +1,23 @@
-export default function() {
+import "../assets/index.css"
+
+export default function () {
     return (
-      <nav>
-        
-      </nav>
+        <nav className="w-screen bg-red">
+            <div>
+                <div>
+                    <h3><strong>Job</strong><span>Search</span></h3>
+                    <h4>menu</h4>
+                </div>
+                <ul>
+                    <li>jobs</li>
+                    <li>companies</li>
+                    <li>about</li>
+                    <li>contact</li>
+                    <li>blog</li>
+                    <li>login</li>
+                    <li>register</li>
+                </ul>
+            </div>
+        </nav>
     )
-  }
+}
