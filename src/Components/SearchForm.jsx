ebd36641ab716c4e6c0a208cca236d19946163cc
cleverAkanimoh <crushclever1@gmail.com>
2023-02-09 13:35:32 +0100

    modified SearchForm.jsx in Components

--- src/Components/SearchForm.jsx
+++ src/Components/SearchForm.jsx
@@ -1,21 +1,26 @@
-import { BsXCircle, BsSearch, BsHouse } from "react-icons/bs"
+import { BsXCircle, BsSearch, BsHouse } from "react-icons/bs";
+import { VscLocation } from "react-icons/vsc";
 
 export default function () {
 	return (
-		<article>
-			<form>
-				<div>
-					<BsSearch />
-					<BsXCircle />
+		<article className="w-1/2 border">
+			<form className="sm:flex">
+				<div className="flex">
+					<BsSearch className="" />
+					<input type="text" name="position" placeholder="Position" />
+					<BsXCircle className="cursor-pointer" />
 				</div>
 
-				<div>
-					<BsHouse />
-					<BsXCircle />
+				<div className="flex">
+					<BsHouse className="" />
+					<input type="text" name="country" placeholder="Country" />
+					<BsXCircle className="cursor-pointer" />
 				</div>
 
-				<div>
-					<BsXCircle />
+				<div className="flex">
+					<VscLocation className="" />
+					<input type="text" name="location" placeholder="Location" />
+					<BsXCircle className="cursor-pointer" />
 				</div>
 
 				<button>search</button>
