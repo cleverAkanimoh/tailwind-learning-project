766350cd01f0a97c30a008dd837b1db5bfc2be20
cleverAkanimoh <crushclever1@gmail.com>
2023-02-09 12:32:50 +0100

    auto-commit

--- src/Components/SearchForm.jsx
+++ src/Components/SearchForm.jsx
@@ -1,14 +1,14 @@
-export default function() {
-    return (
-      <article>
-        <form>
-            <div>
-                
-            </div>
-            <div></div>
-            <div></div>
-            <button>search</button>
-        </form>
-      </article>
-    )
-  }
+export default function () {
+  return (
+    <article>
+      <form>
+        <div>
+
+        </div>
+        <div></div>
+        <div></div>
+        <button>search</button>
+      </form>
+    </article>
+  )
+}
