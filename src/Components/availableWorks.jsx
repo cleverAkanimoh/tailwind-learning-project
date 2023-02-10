d596c3241bb0831675d06cdba0d54687cf23958f
cleverAkanimoh <crushclever1@gmail.com>
2023-02-11 00:01:04 +0100

    modified availableWorks.jsx in Components

--- src/Components/availableWorks.jsx
+++ src/Components/availableWorks.jsx
@@ -1,28 +1,30 @@
 import { BsClock } from "react-icons/bs"
 export default function ({ works }) {
     return (
-        <article className="w-full">
+        <article className="w-full p-3">
             {
                 works.map(work => {
                     const { id, title, location, desc, companyName, url } = work;
 
-                    return <div key={id}>
-                        <div>
+                    return (
+                        <div key={id} className="grid gap-2">
                             <div>
-                                <h2>{title}</h2>
-                                <span>
-                                    <BsClock />
-                                </span>
+                                <div>
+                                    <h2>{title}</h2>
+                                    <span>
+                                        <BsClock />
+                                    </span>
+                                </div>
+                                <span>{location}</span>
                             </div>
-                            <span>{location}</span>
-                        </div>
 
-                        <div>
-                            <p>{desc}</p>
-                            <span>{companyName}</span>
-                            <a href={url}></a>
+                            <div>
+                                <p>{desc}</p>
+                                <span>{companyName}</span>
+                                <a href={url}></a>
+                            </div>
                         </div>
-                    </div>
+                    )
                 })
             }
         </article>
