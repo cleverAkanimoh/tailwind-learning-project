81ec7717497d5094dc641707c6882147024a87e4
cleverAkanimoh <crushclever1@gmail.com>
2023-02-11 00:58:50 +0100

    modified availableWorks.jsx in Components

--- src/Components/availableWorks.jsx
+++ src/Components/availableWorks.jsx
@@ -1,26 +1,26 @@
 import { BsClock } from "react-icons/bs"
 export default function ({ works }) {
     return (
-        <article className="p-2 pt-5 grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 grid gap-5 place-items-center">
+        <article className="p-2 pt-5 grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 grid gap-6 place-items-center">
             {
                 works.map(work => {
                     const { id, title, location, desc, companyName, url } = work;
 
                     return (
-                        <div key={id} className="w-64 border p-3 rounded">
-                            <div>
+                        <div key={id} className="w-11/12 lg:w-64 border p-3 rounded shadow">
+                            <div className="border-b-2 pb-2 mb-2">
                                 <div className="flex justify-between items-center">
-                                    <h2 className="font-extrabold">{title}</h2>
-                                    <span className="flex items-center text-gray-600">
+                                    <h2 className="font-extrabold text-black">{title}</h2>
+                                    <span className="flex items-center text-gray-400">
                                         <BsClock />Now
                                     </span>
                                 </div>
-                                <span>{location}</span>
+                                <span className="font-thin text-sm text-gray-500">{location}</span>
                             </div>
 
                             <div className="flex flex-col">
-                                <p>{desc}</p>
-                                <span>{companyName}</span>
+                                <p className="text-sm text-gray-600 mb-3 mt-2">{desc}</p>
+                                <span className="font-extrabold text-sm mb-3">{companyName}</span>
                                 <a href={url} className="border rounded font-extrabold grid place-items-center p-2">Apply Now</a>
                             </div>
                         </div>
