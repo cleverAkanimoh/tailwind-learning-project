50a92476de7231c2c13ca49ca4ba2039b4c0c591
cleverAkanimoh <crushclever1@gmail.com>
2023-02-10 02:32:06 +0100

    modified NavBar.jsx and index.css

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -2,25 +2,26 @@ import { useState } from "react";
 import { VscMenu } from "react-icons/vsc";
 
 export default function () {
-    const [visibility, setVisibility] = useState(true)
+    const [visibility, setVisibility] = useState(false)
 
-    const toggleNavList =() => {
+    const show = "mt-4 md:mt-0 md:flex justify-center items-center"
+    const hidden = "hidden md:mt-0 md:flex justify-center items-center"
+
+    const toggleNavList = () => {
         setVisibility(prevState => !prevState)
-    }
-
-    const styles = {
-        
     }
 
     return (
         <nav className="w-full sm:p-5 md:p-8 p-3 flex items-center justify-center capitalize">
             <div className="w-full md:flex lg:w-10/12">
-                <div className="sm:w-full flex justify-between">
+                <div className="sm:w-full flex items-center justify-between">
                     <h3 className="lg:text-3xl md:text-2xl text-xl"><strong className="text-blue-700">Job</strong><span className="text-blue-600 text-">Search</span></h3>
-                    <VscMenu onClick={toggleNavList} className="md:hidden cursor-pointer" />
+                    <button onClick={toggleNavList} className="md:hidden cursor-pointer p-3 border rounded">
+                        <VscMenu />
+                    </button>
                 </div>
-                
-                <ul id={visibility ? 'show':'hidden'} className="mt-4 md:mt-0 md:flex justify-center items-center">
+
+                <ul className={visibility ? show : hidden}>
                     <li className="md:mr-4 p-1 hover:text-blue-700 w-auto text-gray-500 font-sans cursor-pointer">jobs</li>
                     <li className="md:mr-4 p-1 hover:text-blue-700 w-auto text-gray-500 font-sans cursor-pointer">companies</li>
                     <li className="md:mr-4 p-1 hover:text-blue-700 w-auto text-gray-500 font-sans cursor-pointer">about</li>
