5f296c0314f56dd11f8816a1e60a60a97d03beb2
cleverAkanimoh <crushclever1@gmail.com>
2023-02-10 22:35:50 +0100

    modified NavBar.jsx and index.css

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -4,8 +4,8 @@ import { VscMenu } from "react-icons/vsc";
 export default function () {
     const [visibility, setVisibility] = useState(false)
 
-    const show = "translate-y-0 transition mt-4 md:mt-0 md:flex justify-center items-center"
-    const hidden = "translate-y-full hidden transition md:mt-0 md:flex justify-center items-center"
+    const show = "mt-4 md:mt-0 md:flex justify-center items-center";
+    const hidden = "hidden md:mt-0 md:flex justify-center items-center";
 
     const toggleNavList = () => {
         setVisibility(prevState => !prevState)
@@ -15,7 +15,7 @@ export default function () {
         <nav className="w-full sm:p-5 md:p-8 p-3 flex items-center justify-center capitalize">
             <div className="w-full md:flex lg:w-10/12">
                 <div className="sm:w-full flex items-center justify-between">
-                    <h3 className="lg:text-3xl md:text-2xl text-xl"><strong className="text-blue-700">Job</strong><span className="text-blue-600 text-">Search</span></h3>
+                    <h3 className="lg:text-3xl md:text-2xl text-xl cursor-pointer"><strong className="text-blue-700">Job</strong><span className="text-blue-600 text-">Search</span></h3>
                     <button onClick={toggleNavList} className="md:hidden cursor-pointer p-3 border rounded">
                         <VscMenu />
                     </button>
