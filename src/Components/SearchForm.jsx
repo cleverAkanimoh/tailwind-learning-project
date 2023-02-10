dfee45a1760017a08936bd9e1d112a342d1b6884
cleverAkanimoh <crushclever1@gmail.com>
2023-02-10 23:25:15 +0100

    modified SearchForm.jsx in Components

--- src/Components/SearchForm.jsx
+++ src/Components/SearchForm.jsx
@@ -25,8 +25,8 @@ export default function () {
 	}
 
 	return (
-		<article className="lg:w-2/2 mt-4 lg:p-10 p-6 bg-blue-100 bg-opacity-50 rounded">
-			<div className="bg-white bg-opacity-25 lg:justify-between p-6 md:flex-row flex flex-col items-center justify-center rounded">
+		<article className="lg:w-12/12 mt-4 lg:p-10 lg:pr-20 lg:pl-20 md:p-10 p-5 sm:p-10 bg-gray-200 bg-opacity-50 rounded">
+			<div className="bg-white bg-opacity-25 lg:justify-between p-3 md:p-3 lg:p-4 md:flex-row flex flex-col items-center justify-center rounded">
 				<div className="flex items-center mb-4 md:mb-0 md:mr-5">
 					<BsSearch className=" text-gray-600" />
 					<input
@@ -82,6 +82,35 @@ export default function () {
 				<button className="bg-blue-400 text-white w-20 h-8 md:w-28 md:h-12 rounded">
 					search
 				</button>
+			</div>
+
+			<div className="text-gray-700 flex flex-col justify-center items-center p-4 md:p-6 md:flex-row">
+				<label className="mb-2 lg:mr-8 md:mb-0 md:mr-6">Sort by: 
+					<select name="sortField" className="capitalize bg-transparent">
+						<option value="relevance">relevance</option>
+						<option value="inclusive">inclusive</option>
+						<option value="starts with">starts with</option>
+						<option value="contains">contains</option>
+					</select>
+				</label>
+
+				<label className="mb-2 lg:mr-8 md:mb-0 md:mr-6">Type: 
+					<select name="typeField" className="capitalize bg-transparent">
+						<option value="full">full-time</option>
+						<option value="part">part-time</option>
+					</select>
+				</label>
+
+				<label  className="mb-2 lg:mr-8 md:mb-0 md:mr-6">Level: 
+					<select name="levelField" className="capitalize bg-transparent">
+						<option value="senior">senior</option>
+						<option value="junior">junior</option>
+						<option value="intermediate">intermediate</option>
+						<option value="advance">advance</option>
+					</select>
+				</label>
+
+				<label className="text-gray-500 cursor-pointer">clear all</label>
 			</div>
 		</article>
 	);
