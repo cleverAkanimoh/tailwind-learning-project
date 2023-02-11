c8515c347d7e17ac521144bb01c3146f67880d68
cleverAkanimoh <crushclever1@gmail.com>
2023-02-11 01:49:02 +0100

    modified MiddleContent.jsx added switchCareer.jsx

--- src/Components/MiddleContent.jsx
+++ src/Components/MiddleContent.jsx
@@ -1,14 +1,18 @@
 import { useState } from 'react';
-import SearchForm from "./SearchForm";
-import AvailableWorks from "./availableWorks";
-import data from "../Components/works";
+// import SearchForm from "./SearchForm";
+// import AvailableWorks from "./availableWorks";
+import data from "./works";
+import SwitchCareer from './switchCareer';
 
 export default function () {
   const [works, setWorks] = useState(data)
   return (
-    <section>
-      <SearchForm />
-      <AvailableWorks works={works} />
+    <section className='w-full grid place-items-center'>
+      <div className='w-11/12'>
+        {/* <SearchForm />
+      <AvailableWorks works={works} /> */}
+        <SwitchCareer />
+      </div>
     </section>
   )
 }
---
+++ src/Components/switchCareer.jsx
@@ -0,0 +1,15 @@
+export default function() {
+    return (
+        <article className="w-full grid place-items-center">
+            <div className="w-full bg-gray-200 bg-opacity-75 p-5 md:p-14 flex flex-col md:flex-row md:justify-between sm:w-11/12 items-center justify-center rounded">
+                <div>
+                    <h2 className="font-extrabold sm:text-lg md:text-xl text-blue-600">Ready to switch a career?</h2>
+                    <h4 className="font-extrabold text-sm sm:text-base md:text-lg">Let's get started!</h4>
+                </div>
+                <div className="mt-5 md:mt-0">
+                    <a href="#" className="border-blue-500 shadow text-blue-600 border p-2 text-sm rounded">Get Started</a>
+                </div>
+            </div>
+        </article>
+    )
+}
